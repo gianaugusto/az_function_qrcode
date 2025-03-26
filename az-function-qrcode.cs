e18614d06ea53fac0b02e59a96140cbd401883eb
Giancarlos Macedo <gianaugusto@gmail.com>
2025-03-26 21:52:25 +0000

    fix: set default value

--- az-function-qrcode.cs
+++ az-function-qrcode.cs
@@ -22,22 +22,23 @@ namespace az_function_qrcode
         }
 
         [FunctionName("az_function_qrcode")]
-        [OpenApiOperation(operationId: "Run", tags: new[] { "name" })]
+        [OpenApiOperation(operationId: "Run", tags: new[] { "content" })]
         [OpenApiSecurity("function_key", SecuritySchemeType.ApiKey, Name = "code",
             In = OpenApiSecurityLocationType.Query)]
-        [OpenApiParameter(name: "name", In = ParameterLocation.Query, Required = true, Type = typeof(string),
-            Description = "The **Name** parameter")]
+        [OpenApiParameter(name: "content", In = ParameterLocation.Query, Required = true, Type = typeof(string),
+            Description = "The **content** parameter")]
         [OpenApiResponseWithBody(statusCode: HttpStatusCode.OK, contentType: "text/plain", bodyType: typeof(string),
             Description = "The OK response")]
         public async Task<IActionResult> Run(
             [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)]
             HttpRequest req)
         {
-            _logger.LogInformation("C# HTTP trigger function processed a request.");
+            _logger.LogInformation("C# HTTP trigger function processed a request to generate new qrcode.");
 
             string content = req.Query["content"];
+            var qrCodeBase64 = QrCode.GenerateQrCodeBase64(content ?? "Teste 123");
 
-            return new OkObjectResult(QrCode.GenerateQrCodeBase64(content ?? "teste 123"));
+            return new OkObjectResult($"data:image/png;base64,{qrCodeBase64}");
         }
     }
 }
